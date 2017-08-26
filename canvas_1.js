461de949ea5da410e01b1bdc411a11fad766c898
Rodger Kurth <rocknraj@gmail.com>
2017-08-26 13:00:51 -0500

    Adds animation

--- canvas_1.js
+++ canvas_1.js
@@ -6,11 +6,25 @@ myCanvasAnimation = () => {
     let myCanvas = document.getElementById("myCanvas");
     let context = myCanvas.getContext("2d");
 
+    initialX = 60;
+    finalX = 200; // start and end points for animation
+
     drawCanvas = () => {
+        context.clearRect(0,0,300,300); // stops moving circle from leaving a "trail"
         context.beginPath();
-        context.arc(50,50,40,0,2*Math.PI);
+        context.arc(initialX, 60, 50, 0, 2*Math.PI, true);
+        context.fillStyle = "#00f";
+        context.fill();
         context.stroke();
+
+        if (initialX < finalX ) {
+            initialX = initialX + 1;
+        }
+
+        window.requestAnimationFrame(drawCanvas);
+
     }
+
 
     drawCanvas();
 }
