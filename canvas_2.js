91471457a591144aeaaf4d0d587e5c1418736bd9
Rodger Kurth <rocknraj@gmail.com>
2017-09-06 07:16:06 -0500

    Adding canvas text and lines

--- canvas_2.js
+++ canvas_2.js
@@ -9,5 +9,12 @@ myCanvasAnimation = () => {
 		
 		context.fillStyle = "#ff1a1a";
 		// note that fillStyle is a property not a method
-		context.fillRect(0, 0, 300, 200);
+		context.fillRect(0, 0, 300, 300);
+		context.moveTo(0,0);
+		context.lineTo(300,300);
+		context.stroke();
+
+		context.moveTo(300,0);
+		context.lineTo(0,300);
+		context.stroke();
 }	
